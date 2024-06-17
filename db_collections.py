429aea883fb2770db23e6246898612c19a213e9b
Isaiah Richards <isaiah@tablz.com>
2024-06-17 17:03:00 -0400

    linked database

--- db_collections.py
+++ db_collections.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
 
 # add client string
-db = MongoClient()
+db = MongoClient(
+    "mongodb+srv://isaiah:Nadder3415@fleetvision.tmugdte.mongodb.net/?retryWrites=true&w=majority&appName=FleetVision"
+)
 
 # add db name
-CapstoneDB = db.get_database("")
+CapstoneDB = db.get_database("fleet-vision")
 
 obd_col = CapstoneDB.get_collection("obd")
 model_col = CapstoneDB.get_collection("model")
