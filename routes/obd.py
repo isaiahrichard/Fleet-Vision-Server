115ab97e588473d54bf7622c7db88a9bb1e60780
Naumaan <naumaansheikh30@gmail.com>
2024-07-17 06:33:22 -0400

    Added code for TCP server to communicate with Boron MCU. Uses a shared Queue with /obd to retrieve data

--- routes/obd.py
+++ routes/obd.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, request, make_response
 from db_collections import obd_col
 import requests
+import tcp_server
 
 obd = Blueprint("obd", __name__)
 
 
 @obd.route("/obd", methods=["GET"])
 def get_obd():
+    # Get data from shared queue with TCP server
+    # data = tcp_server.data_queue.get()
+    
     return make_response("Request for OBD", 200)
--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from routes.obd import obd
 from routes.model import model
+import threading
+import tcp_server
 
 app = Flask(__name__)
 app.register_blueprint(obd)
 app.register_blueprint(model)
 
-
 if __name__ == "__main__":
+    # start TCP and Flask server on separate threads
+    # tcp_thread = threading.Thread(target=tcp_server.start_tcp_server)
+    # tcp_thread.start()
+    
     app.run(debug=True)
