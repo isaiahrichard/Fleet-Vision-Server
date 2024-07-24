454fbe25f9a65d6cc2c7d6da0b628ca46946d90f
Aaron Spence <aaronspence31@gmail.com>
2024-07-24 13:48:44 -0400

    initial work to support two video streams, but too slow

--- routes/stream_viewer.py
+++ routes/stream_viewer.py
@@ -18,29 +18,38 @@ logger = logging.getLogger(__name__)
 
 # Constants
 BATCH_SIZE = 5
-PREDICTION_THRESHOLD = 0.7
+BINARY_EYES_STATE_PREDICTION_THRESHOLD = 0.3
+BINARY_DISTRACTION_THRESHOLD = 0.3
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 model_dir = os.path.join(base_dir, "models")
 
 # Load models and label mappings
 try:
-    eyes_model = load_model(
-        os.path.join(model_dir, "face_eyes_state_MobileNetV3Small_16_30epochs")
+    binary_eyes_state_model = load_model(
+        os.path.join(
+            model_dir, "binary_MobileNetV3Small_32batchsize_1e-06learningrate_4epochs"
+        )
     )
-    actions_model = load_model(
-        os.path.join(model_dir, "body_driver_actions_MobileNetV3Small_32_3epochs")
+    binary_distraction_model = load_model(
+        os.path.join(
+            model_dir, "binary_MobileNetV3Small_32batchsize_1e-06learningrate_4epochs"
+        )
     )
 
-    with open(os.path.join(model_dir, "label_mapping_face_eyes_state.json"), "r") as f:
+    with open(
+        os.path.join(model_dir, "label_mapping_binary_distraction.json"), "r"
+    ) as f:
         eyes_label_to_index = json.load(f)
     with open(
-        os.path.join(model_dir, "label_mapping_body_driver_actions.json"), "r"
+        os.path.join(model_dir, "label_mapping_binary_distraction.json"), "r"
     ) as f:
-        actions_label_to_index = json.load(f)
+        distraction_label_to_index = json.load(f)
 
     eyes_index_to_label = {int(v): k for k, v in eyes_label_to_index.items()}
-    actions_index_to_label = {int(v): k for k, v in actions_label_to_index.items()}
+    distraction_index_to_label = {
+        int(v): k for k, v in distraction_label_to_index.items()
+    }
 
     logger.info("Models and label mappings loaded successfully")
 except Exception as e:
@@ -55,19 +64,24 @@ def preprocess_image(frame, target_size=(224, 224)):
     return normalized_rgb_img
 
 
-def predict_batch(batch, model, index_to_label):
+def predict_batch(batch, model, index_to_label, is_distraction_model=False):
     predictions = model.predict(np.array(batch), batch_size=BATCH_SIZE)
     predicted_labels = []
     for pred in predictions:
-        max_prob = np.max(pred)
-        if max_prob >= PREDICTION_THRESHOLD:
-            predicted_labels.append(index_to_label[np.argmax(pred)])
+        if is_distraction_model:
+            if pred[0] >= BINARY_DISTRACTION_THRESHOLD:
+                predicted_labels.append(index_to_label[1])  # Safe driving
+            else:
+                predicted_labels.append(index_to_label[0])  # Distracted
         else:
-            predicted_labels.append("Unknown")
+            if pred[0] >= BINARY_EYES_STATE_PREDICTION_THRESHOLD:
+                predicted_labels.append(index_to_label[1])  # Eyes open
+            else:
+                predicted_labels.append(index_to_label[0])  # Eyes closed
     return predicted_labels
 
 
-def process_stream(stream_url):
+def process_stream(stream_url, model, index_to_label, is_distraction_model=False):
     cap = cv2.VideoCapture(stream_url)
     while True:
         batch_frames = []
@@ -81,24 +95,15 @@ def process_stream(stream_url):
 
         if len(batch_frames) == BATCH_SIZE:
             processed_batch = [preprocess_image(frame) for frame in batch_frames]
-            eyes_predictions = predict_batch(
-                processed_batch, eyes_model, eyes_index_to_label
-            )
-            actions_predictions = predict_batch(
-                processed_batch, actions_model, actions_index_to_label
+            predictions = predict_batch(
+                processed_batch, model, index_to_label, is_distraction_model
             )
 
             middle_frame = batch_frames[BATCH_SIZE // 2]
             _, buffer = cv2.imencode(".jpg", middle_frame)
             frame_base64 = base64.b64encode(buffer).decode("utf-8")
 
-            yield (
-                f"data: {{\n"
-                f'data: "image": "{frame_base64}",\n'
-                f'data: "eyes_predictions": {json.dumps(eyes_predictions)},\n'
-                f'data: "actions_predictions": {json.dumps(actions_predictions)}\n'
-                f"data: }}\n\n"
-            )
+            yield f"data: {json.dumps({'image': frame_base64, 'predictions': predictions})}\n\n"
 
 
 @stream_viewer.route("/")
@@ -106,15 +111,30 @@ def index():
     return render_template("index.html")
 
 
-@stream_viewer.route("/video_feed")
-def video_feed():
-    face_stream_url = "http://192.168.2.175/stream"  # Adjust if needed
-    return Response(process_stream(face_stream_url), mimetype="text/event-stream")
+@stream_viewer.route("/face_stream")
+def face_stream():
+    face_stream_url = "http://172.20.10.4/stream"  # Adjust if needed
+    return Response(
+        process_stream(face_stream_url, binary_eyes_state_model, eyes_index_to_label),
+        mimetype="text/event-stream",
+    )
+
+
+@stream_viewer.route("/body_stream")
+def body_stream():
+    body_stream_url = "http://172.20.10.5/stream"  # Adjust if needed
+    return Response(
+        process_stream(
+            body_stream_url, binary_distraction_model, distraction_index_to_label, True
+        ),
+        mimetype="text/event-stream",
+    )
 
 
 @stream_viewer.route("/stream_info", methods=["GET"])
 def get_stream_info():
     return {
-        "batch_size": BATCH_SIZE,
-        "prediction_threshold": PREDICTION_THRESHOLD,
+        "BATCH_SIZE": BATCH_SIZE,
+        "BINARY_EYES_STATE_PREDICTION_THRESHOLD": BINARY_EYES_STATE_PREDICTION_THRESHOLD,
+        "BINARY_DISTRACTION_THRESHOLD": BINARY_DISTRACTION_THRESHOLD,
     }
--- server.py
+++ server.py
@@ -26,3 +26,4 @@ if __name__ == "__main__":
     # Run the Flask app
     logger.info("Starting Flask app...")
     app.run(debug=False, host="0.0.0.0", port=5000, use_reloader=False)
+    logger.info("Server stopped.")  # This line will execute when the server stops
